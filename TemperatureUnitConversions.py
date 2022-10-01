05ddc754a386ea2618758336d2d37d012f6e89a2
Timmurray <murrayt132@gmail.com>
2022-10-01 15:24:31 -0400

    changes to make the methods callable and usable

--- TemperatureUnitConversions.py
+++ TemperatureUnitConversions.py
@@ -8,24 +8,24 @@ import math
 
 convertCelsiusToKelvin(celsius):
     temperature = celsius + 273.15
-    return temperature + ' K'
+    return temperature
 
 convertCelsiusToFarenheight(celsius):
     temperature = (9/5)*celsius + 32
-    return temperature + ' F'
+    return temperature
 
 convertFarenheightToCelsius(farenheight):
     temperature = (5/9)*farenheight - 32
-    return temperature + ' C'
+    return temperature
     
 convertFarenheightToKelvin(farenheight):
     temperature = (5/9)*farenheight + 241.15
-    return temperature + ' K'
+    return temperature
 
 convertKelvinToCelsius(kelvin):
     temperature = kelvin - 273.15
-    return temperature + ' C'
+    return temperature 
     
 convertKelvinToFarenheight(kelvin):
     temperature = (9/5)*(kelvin - 273.15) + 32
-    return temperature + ' F'
+    return temperature
