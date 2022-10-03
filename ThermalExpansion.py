269b66cbd28af07ec485b582b54d3d2fb813c10a
Timmurray <murrayt132@gmail.com>
2022-10-03 13:01:27 -0400

    Update ThermalExpansion.py

--- ThermalExpansion.py
+++ ThermalExpansion.py
@@ -5,11 +5,36 @@ import TemperatureUnitConversions.py
 # the known coefficients of linear expansion from most materials will exist in a dictionary
 # the known  coefficients of volume expanison for most materials will exist in a dictionary
 
-CoefficientLinearExpansion(FinalLength, InitialLength, FinalTemperature, InitialTemperature):
-    alpha = ((FinalLength-InitialLength)/(FinaiTemperature-InitialTemperature))/(InitialLength)
+coefficientLinearExpansion(finalLength, initialLength, finalTemperature, initialTemperature):
+    alpha = ((finalLength-initialLength)/(finaiTemperature-initialTemperature))/(initialLength)
     return alpha
 
-CoefficientVolumeExpansion(initialVolume, finalVolume, initialTemperature, finalTemperature):
+#solving initial length of material with known coefficient of linear expansion, final temperature, initial temperature, and final length
+initialLengthLinearExpansion(alpha, finalTemperature, initialTemperature, finalLength):
+    initialLength = (finalLength)/(alpha*(finalTemperature-initialTemperature)+1)
+    return initialLength
+
+#solving the final length of a material with known coefficient of linear expansion, final temperature, initial temperature, and initial length
+finalLengthLinearExpansion(alpha, finalTemperature, initialTemperature, initialLength):
+    finalLength = (initialLength*alpha*(finalTemperature-initialTemperature)) + 1
+    return finalLength
+
+#solving initial temperature in Kelvin with coefficient of linear expansion, initial length, final length, and final temperature
+
+initialTemperatureLinearExpansion(alpha, initialLength, finalLength, finalTemperature):
+    initialTemperature = -((finalLength-initialLength)/(alpha*initialLength)) + finalTemperature
+    return initialTemperature
+
+#solving final temperature in Kelvin with coefficient of linear expansion, initial length, final length, and initial temperature
+
+finalTemperatureLinearExpansion(alpha, initialLength, finalLength, initialTemperature):
+    finalTemperature = (finalLength-initialLength)/(alpha*initialLength) + initialTemperature
+    return finalTemperature
+
+# solving for beta the coefficient of volume expansion with known initial volume, final volume, initial temperature, and final temperature
+
+coefficientVolumeExpansion(initialVolume, finalVolume, initialTemperature, finalTemperature):
     beta = (1/initialVolume)*((finalVolume-initialVolume)/(finalTemperature-initialTemperature))
     return beta
 
+#solving for the 
