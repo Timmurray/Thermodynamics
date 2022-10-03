f0eabddba29eb0c64a844c76882883d48b41c880
Timmurray <murrayt132@gmail.com>
2022-10-03 17:08:46 -0400

    Update ThermalExpansion.py

--- ThermalExpansion.py
+++ ThermalExpansion.py
@@ -37,4 +37,26 @@ coefficientVolumeExpansion(initialVolume, finalVolume, initialTemperature, final
     beta = (1/initialVolume)*((finalVolume-initialVolume)/(finalTemperature-initialTemperature))
     return beta
 
-#solving for the 
+#solving for the initial volume using the volume expansion formula with known beta, final volume, initial temperature, and final temperature
+
+initialVolumeVolumeExpansion(beta, finalVolume, initialTemperature, finalTemperature):
+    initialVolume = (finalVolume)/(alpha*(finalTemperature-initialTemperature)+1)
+    return initialVolume
+
+#solving for final volume using the volume expansion formula with known beta coefficient of volume expansion, initial volume, initial temperature and final temperature
+
+finalVolumeVolumeExpansion(beta, initialVolume, initialTemperature, finalTemperature):
+    finalVolume = (initialTemperature*alpha*(finalTemperature-initialTemperature)) + 1
+    return finalVolume
+
+#solving for initial temperature using the volume expansion formula with a known beta, initial volume, final volume, and final temperature
+
+initialTemperatureVolumeExpansion(beta, initialVolume, finalVolume, finalTemperature):
+    initialTemperature = -((finalVolume-initialVolume) / (beta*initialVolume)) + finalTemperature
+    return initialTemperature
+
+#solving for final temperature using the volume expansion formula with known beta, initial volume, final volume, and initial temperature
+
+finalTemperatureVolumeExpansion(beta, initialVolume, finalVolume, initialTemperature):
+    finalTemperature = (finalVolume-initialVolume)/(beta*initialVolume) + initialTemperature
+    return finalTemperature
